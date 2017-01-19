2f3776944224dc7596a199baa3f44057b2c6a2ac
Robert Leggett <robert.leggett2@auspost.com.au>
2017-01-19 15:43:52 +1100

    Adding examples to follow

--- client/app/action/user/actions.js
+++ client/app/action/user/actions.js
@@ -10,3 +10,6 @@ fetch(CUSTOMER_ENDPOINT)
 
 // https://auth0.com/blog/secure-your-react-and-redux-app-with-jwt-authentication/
 // http://redux.js.org/docs/api/createStore.html
+// http://www.thegreatcodeadventure.com/jwt-authentication-with-react-redux/github melb
+
+// https://github.com/auth0-blog/redux-auth
