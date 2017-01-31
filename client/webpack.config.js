711e0896c2a538b84d8312bb8676358c49226572
Robert Leggett <robert.leggett2@auspost.com.au>
2017-01-31 13:58:20 +1100

    Webpack 2 upgrade

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,7 +7,7 @@ const BUILD_DIR = path.resolve(__dirname, 'public');
 const APP_DIR = path.resolve(__dirname, 'app');
 
 let generateHtml = new HtmlWebpackPlugin({ title: 'My App' });
-let extractCSS = new ExtractTextPlugin('styles/[name].css', { allChunks: true });
+let extractCSS = new ExtractTextPlugin({ filename: 'styles/[name].css', allChunks: true });
 
 const config = {
   entry: APP_DIR + '/index.js',
@@ -25,11 +25,21 @@ const config = {
       {
         test : /\.jsx?/,
         include : APP_DIR,
-        loader : 'babel'
+        loader : 'babel-loader'
       },
       {
         test: /\.scss$/,
-        loader: extractCSS.extract('style', 'css?modules=true!sass?sourceMap=true')
+        loader: extractCSS.extract({
+          loader: [
+            {
+              loader: "css-loader",
+              options: { modules: true }
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
+        })
       }
     ]
   },
